ce1c3283ebda78ba0abee916a68c4d87e36b237d
Thomas Ngo <thoas015@gmail.com>
2018-05-21 22:07:12 -0400

    5/21 10:00PM Incomplete Build

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,25 +1,25 @@
 package virtualpet;
 
 public class VirtualPet {
-
-	// Instance Variables (Pet Stats)
 	
-	private String name;
-	private int statusNutrition;
-	private int statusMood;
-	private int statusEnergy;
-	
-	// Pet Status
-
-	// Constructor
+	/* Constructor */
 
 	public VirtualPet (String name) {
 		this.name = name;
-		this.statusNutrition = 10;
-		this.statusMood = 10;
-		this.statusEnergy = 10;
+		this.statNutrition = 10;
+		this.statMood = 10;
+		this.statEnergy = 10;
 	}
 	
+
+	/* Pet Stats */
+	
+	private String name;
+	private int statNutrition;
+	private int statMood;
+	private int statEnergy;
+	
+
 	/* Get/Set Methods */
 	
 	// Name
@@ -30,40 +30,101 @@ public class VirtualPet {
 	
 	// Nutrition
 	
-	public int getStatusHunger() {
-		return statusNutrition;
+	public int getStatNutrition() {
+		return statNutrition;
 	}
 	
-	public void setStatusHunger(int amount) {
-		this.statusNutrition = amount;
+	public void setStatNutrition(int amount) {
+		this.statNutrition = amount;
 	}
 		
-	public void subtractStatusHunger(int amount) {
-		if (this.statusNutrition < amount) {
-			this.statusNutrition -= this.statusNutrition;
+	public void subtractStatNutrition(int amount) {
+		if (this.statNutrition < amount) {
+			this.statNutrition -= this.statNutrition;
 		} else {
-			this.statusNutrition -= amount;
+			this.statNutrition -= amount;
 		}
 	}
 	
-	public void addStatusHunger(int amount) {
-		if (this.statusNutrition > amount) {
-			this.statusNutrition -= this.statusNutrition;
-		} else {
-			this.statusNutrition -= amount;
+	public void addStatNutrition(int amount) {
+		this.statNutrition += amount;
+		if (this.statNutrition > 16) {
+			this.statNutrition = 16;
 		}
 	}
 	
 	// Mood
 	
-	public int getStatusMood() {
+	public int getStatMood() {
+		return statMood;
+	}
+	
+	public void setStatMood(int amount) {
+		this.statMood = amount;
+	}
 		
+	public void subtractStatMood(int amount) {
+		if (this.statMood < amount) {
+			this.statMood -= this.statMood;
+		} else {
+			this.statMood -= amount;
+		}
+	}
+	
+	public void addStatMood(int amount) {
+		this.statMood += amount;
+		if (this.statMood > 16) {
+			this.statMood = 16;
+		}
+	}
+	
+	// Energy
+	
+	public int getStatEnergy() {
+		return statEnergy;
+	}
+	
+	public void setStatEnergy(int amount) {
+		this.statEnergy = amount;
+	}
+		
+	public void subtractStatEnergy(int amount) {
+		if (this.statEnergy < amount) {
+			this.statEnergy -= this.statEnergy;
+		} else {
+			this.statEnergy -= amount;
+		}
+	}
+	
+	public void addStatEnergy(int amount) {
+		this.statEnergy += amount;
+		if (this.statEnergy > 16) {
+			this.statEnergy = 16;
+		}
+	}
+	
+
+	/* Behavior Methods */
+	
+	public void inputFeed() {
+		this.addStatNutrition(6);
+		this.addStatMood(2);
+	}
+	
+	public void inputPlay() {
+		this.addStatMood(6);
+		this.subtractStatEnergy(2);
+		this.subtractStatNutrition(2);
+	}
+	
+	public void inputSleep() {
+		this.addStatEnergy(6);
+		this.addStatMood(2);
+		this.subtractStatNutrition(2);
 	}
 	
 	
-
-	// Behavior Methods
 	
-	// Tick
+	/* On Tick */
 
 }
--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -1,3 +1,6 @@
+//INCOMPLETE
+//Please don't grade me on this :D
+
 package virtualpet;
 
 import static org.junit.Assert.assertEquals;
@@ -8,7 +11,7 @@ import virtualpet.VirtualPet;
 
 public class VirtualPetTest {
 
-	// Status Tests
+	// Stat Tests
 
 	@Test
 	public void constructorShouldSetName() {
@@ -18,32 +21,80 @@ public class VirtualPetTest {
 	}                        
 	
 	@Test
-	public void constructorDefualtHungerShouldBe10() {
+	public void constructorDefualtNutritionShouldBe10() {
 		VirtualPet underTest = new VirtualPet("Test");
-		int hunger = underTest.getStatusHunger();
+		int hunger = underTest.getStatNutrition();
 		assertEquals(10, hunger);
 	}
 	
-	@Test
-	public void constructorDefualtMoodShouldBe10() {
-		VirtualPet underTest = new VirtualPet("Test");
-		int mood = underTest.getStatusMood();
-		assertEquals(10, mood);
-	}
 
 	@Test
 	public void constructorDefualtEnergyShouldBe10() {
 		VirtualPet underTest = new VirtualPet("Test");
-		int energy = underTest.getStatusM
+		int energy = underTest.getStatNutrition();
+	}			
+				
+	@Test 
+	
+	//copying and pasting code from get/set section for each stat
+	public void addStatAdditionShouldNotExceedMax(){
+			VirtualPet underTest = new VirtualPet("Test");
+			underTest.addStatNutrition(17);
+			int nutrition = underTest.getStatNutrition();
+			assertEquals(16, nutrition);
+		}
+	
+
+	/* Behavior Method Tests */
+
+	@Test
+	public void inputFeedShouldAddNutrition() {
+		VirtualPet underTest = new VirtualPet("Test");
+		underTest.inputFeed();
+		int nutrition = underTest.getStatNutrition();
+		assertEquals(16, nutrition);
 	}
-//	@Test
-//	public void cannotMakeHungerNegativeAfterSubtractingMoreHungerPointsThanCurrent() {
-//		Vpet underTest = new Vpet("Test");
-//		underTest.setStat
-//		
-//		assertEquals(0, underTest.getStatusHunger());
-//	}
-
-
-
+	
+	@Test
+	public void inputFeedShouldAddMood() {
+		VirtualPet underTest = new VirtualPet("Test");
+		underTest.inputFeed();
+		int mood = underTest.getStatMood();
+		assertEquals(12, mood);
+	}
+	
+	@Test
+	public void inputFeedShouldAddNutritionAndNoMoreThanMax() {
+		VirtualPet underTest = new VirtualPet("Test");
+		underTest.setStatNutrition(15);
+		underTest.inputFeed();
+		int nutrition = underTest.getStatNutrition();
+		assertEquals(16, nutrition);
+	}
+	
+	@Test
+	public void statMoodShouldNotGoPast16() {
+		VirtualPet underTest = new VirtualPet("Test");
+		underTest.setStatMood(15);
+		underTest.addStatMood(16);
+		int mood = underTest.getStatMood();
+		assertEquals(16, mood );
+	}
+	
+	@Test
+	public void statEnergyShouldNotGoPast16() {
+		VirtualPet underTest = new VirtualPet("Test");
+		underTest.setStatEnergy(15);
+		underTest.addStatEnergy(16);
+		int energy = underTest.getStatEnergy();
+		assertEquals(16, energy );
+	}
+	
+	// TEST BEHAVIOR METHODS ON 5/22
+	
 }
+	
+
+
+
+
