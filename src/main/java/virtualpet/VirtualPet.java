5217807222528e9326b3513bc3980634d6339a24
Thomas Ngo <thoas015@gmail.com>
2018-05-22 20:40:56 -0400

    5/22 8:40PM Incomplete

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,43 +1,41 @@
 package virtualpet;
 
 public class VirtualPet {
-	
+
 	/* Constructor */
 
-	public VirtualPet (String name) {
+	public VirtualPet(String name) {
 		this.name = name;
 		this.statNutrition = 10;
 		this.statMood = 10;
 		this.statEnergy = 10;
 	}
-	
 
 	/* Pet Stats */
-	
+
 	private String name;
 	private int statNutrition;
 	private int statMood;
 	private int statEnergy;
-	
 
 	/* Get/Set Methods */
-	
+
 	// Name
 
 	public String getName() {
 		return this.name;
 	}
-	
+
 	// Nutrition
-	
+
 	public int getStatNutrition() {
 		return statNutrition;
 	}
-	
+
 	public void setStatNutrition(int amount) {
 		this.statNutrition = amount;
 	}
-		
+
 	public void subtractStatNutrition(int amount) {
 		if (this.statNutrition < amount) {
 			this.statNutrition -= this.statNutrition;
@@ -45,24 +43,24 @@ public class VirtualPet {
 			this.statNutrition -= amount;
 		}
 	}
-	
+
 	public void addStatNutrition(int amount) {
 		this.statNutrition += amount;
 		if (this.statNutrition > 16) {
 			this.statNutrition = 16;
 		}
 	}
-	
+
 	// Mood
-	
+
 	public int getStatMood() {
 		return statMood;
 	}
-	
+
 	public void setStatMood(int amount) {
 		this.statMood = amount;
 	}
-		
+
 	public void subtractStatMood(int amount) {
 		if (this.statMood < amount) {
 			this.statMood -= this.statMood;
@@ -70,24 +68,24 @@ public class VirtualPet {
 			this.statMood -= amount;
 		}
 	}
-	
+
 	public void addStatMood(int amount) {
 		this.statMood += amount;
 		if (this.statMood > 16) {
 			this.statMood = 16;
 		}
 	}
-	
+
 	// Energy
-	
+
 	public int getStatEnergy() {
 		return statEnergy;
 	}
-	
+
 	public void setStatEnergy(int amount) {
 		this.statEnergy = amount;
 	}
-		
+
 	public void subtractStatEnergy(int amount) {
 		if (this.statEnergy < amount) {
 			this.statEnergy -= this.statEnergy;
@@ -95,36 +93,39 @@ public class VirtualPet {
 			this.statEnergy -= amount;
 		}
 	}
-	
+
 	public void addStatEnergy(int amount) {
 		this.statEnergy += amount;
 		if (this.statEnergy > 16) {
 			this.statEnergy = 16;
 		}
 	}
-	
 
 	/* Behavior Methods */
-	
+
 	public void inputFeed() {
 		this.addStatNutrition(6);
 		this.addStatMood(2);
 	}
-	
+
 	public void inputPlay() {
 		this.addStatMood(6);
 		this.subtractStatEnergy(2);
 		this.subtractStatNutrition(2);
 	}
-	
+
 	public void inputSleep() {
 		this.addStatEnergy(6);
 		this.addStatMood(2);
 		this.subtractStatNutrition(2);
 	}
-	
-	
-	
+
 	/* On Tick */
+
+	public void tick() {
+		this.subtractStatMood(1);
+		this.subtractStatEnergy(1);
+		this.subtractStatNutrition(1);
+	}
 
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -1,10 +1,62 @@
 package virtualpet;
 
+import java.util.Scanner;
+
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Welcome, and say \"Hi!\" to your new Virtual Pet!");
+		System.out.println("Give your pet a name!");
+		String nameInput = input.nextLine();
+		VirtualPet pet = new VirtualPet(nameInput);
+
+		System.out.println();
+		System.out.println(pet.getName() + " wags its tail at you. It seems to like that name!\n");
+
+		while (true) {
+
+			int choice = 0;
+
+			for (int i = 0; i < pet.getName().length(); i++) {
+				System.out.print("*");
+			}
+			System.out.println("\n" + pet.getName() + "");
+			for (int i = 0; i < pet.getName().length(); i++) {
+				System.out.print("*");
+			}
+
+			System.out.println("\nNutrition: " + pet.getStatNutrition());
+			System.out.println("Energy: " + pet.getStatEnergy());
+			System.out.println("Mood: " + pet.getStatMood());
+
+			System.out.println("\n" + pet.getName() + " looks at you in anticipation.");
+			System.out.println("What would you like to do with " + pet.getName() + "?\n");
+			System.out.println("1. Feed " + pet.getName());
+			System.out.println("2. Play with " + pet.getName());
+			System.out.println("3. Put " + pet.getName() + " to bed");
+			System.out.println("4. Quit");
+			choice = input.nextInt();
+			input.nextLine();
+
+			if (choice == 1) {
+				pet.inputFeed();
+				System.out.println("\n" + pet.getName() + " happily munches on virtual food. ");
+				System.out.println("You don't quite understand how, but it fills him up.\n");
+				pet.tick();
+			} else if (choice == 2) {
+				pet.inputPlay();
+				System.out.println("\n" + pet.getName() + " chases his own tail as you mysteriously");
+				System.out.println("find yourself pulling out a laser pointer and moving");
+				System.out.println("it in circles, out of your own accord.\n");
+				pet.tick();
+			} else if (choice == 3) {
+				pet.inputSleep();
+				System.out.println("");
+			}
+
+		}
 	}
-
 }
