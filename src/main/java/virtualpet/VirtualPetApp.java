9700c173322bf9812784c8cbd250fe1bc4de2eb1
Thomas Ngo <thoas015@gmail.com>
2018-05-22 22:50:11 -0400

    5/22 Core Complete

--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -51,49 +51,50 @@ public class VirtualPetApp {
 			System.out.println("5. Quit");
 			choice = input.nextLine();
 
-			// Input Results
+			/* Input Results */
 
-			if (choice.equalsIgnoreCase("1")) {
+			// Invalid Option
+
+			while (!(choice.equalsIgnoreCase("1")) && !(choice.equalsIgnoreCase("2")) && !(choice.equalsIgnoreCase("3"))
+					&& !(choice.equalsIgnoreCase("4")) && !(choice.equalsIgnoreCase("5"))) {
+				System.out.println("\n" + pet.getName() + " seems to be confused.");
+				System.out.println("Please enter a valid option.");
+				choice = input.nextLine();
+			}
+
+			// Feed
+			if (choice.equalsIgnoreCase("1")) {			
 				pet.tick();
 				pet.inputFeed();
 				System.out.println("\n" + pet.getName() + " happily munches on virtual food. ");
 				System.out.println("You don't quite understand how, but it fills him up.\n");
-
-			} else if (choice.equalsIgnoreCase("2")) {
+			
+			// Play
+			} else if (choice.equalsIgnoreCase("2")) {	
 				pet.tick();
 				pet.inputPlay();
 				System.out.println("\n" + pet.getName() + " chases his own tail as you mysteriously");
 				System.out.println("find yourself pulling out a laser pointer and moving");
 				System.out.println("it in circles, out of your own accord.\n");
-
-			} else if (choice.equalsIgnoreCase("3")) {
+			
+			// Put to bed
+			} else if (choice.equalsIgnoreCase("3")) {	
 				pet.tick();
 				pet.inputSleep();
 				System.out.println(pet.getName() + " curls up in his mysteriously obtained bed");
 				System.out.println("and goes to sleep. It wakes up later, feeling refreshed.\n ");
 
-			} else if (choice.equalsIgnoreCase("4")) {
+			// Do nothing
+			} else if (choice.equalsIgnoreCase("4")) {	
 				pet.tick();
 				System.out.println(pet.getName() + " sits and looks away, seemingly daydreaming about");
 				System.out.println("eating, playing, and sleeping. \n");
-
+			
+			// Quit
 			} else if (choice.equalsIgnoreCase("5")) {
 				System.out.println(pet.getName() + " wags his tail in farewell, awaiting your return.");
 				input.close();
 				System.exit(0);
-
-			}
-			
-			// Invalid Option
-
-			else {
-				while (!(choice.equalsIgnoreCase("1")) && !(choice.equalsIgnoreCase("2"))
-						&& !(choice.equalsIgnoreCase("3")) && !(choice.equalsIgnoreCase("2"))
-						&& !(choice.equalsIgnoreCase("2"))) {
-					System.out.println("\n" + pet.getName() + " seems to be confused.");
-					System.out.println("Please enter a valid option.");
-					choice = input.nextLine();
-				}
 			}
 
 		}
